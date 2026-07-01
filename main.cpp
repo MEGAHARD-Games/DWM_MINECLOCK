8d8f297a6b3e602adcfc71181fbe4084e30ff2ae
koksiroj <Koksiroj@gmail.com>
2026-07-01 22:46:53 +0200

    sunset/sunrise calculation function (missing call)

--- main.cpp
+++ main.cpp
@@ -4,7 +4,13 @@
 #define SDL_MAIN_USE_CALLBACKS 1  /* use the callbacks instead of main() */
 #include <SDL3/SDL.h>
 #include <SDL3/SDL_main.h>
+#include <chrono>
+#include <ctime>
+#include <cmath>
 
+#define TIMEZONEOFFSET (-5)
+#define LATITUDE 52.219705
+#define LONGITUDE 6.901704
 #define WINDOW_WIDTH 256
 #define WINDOW_HEIGHT 256
 
@@ -20,6 +26,8 @@ static int night_chunk = 0;
 /* This function runs once at startup. */
 SDL_AppResult SDL_AppInit(void **appstate, int argc, char *argv[])
 {
+    Calibrate();
+
     SDL_Surface *surface = NULL;
     char *png_path = NULL;
 
@@ -132,4 +140,136 @@ SDL_AppResult SDL_AppIterate(void *appstate)
 void SDL_AppQuit(void *appstate, SDL_AppResult result)
 {
     SDL_DestroyTexture(texture);
+}
+
+void Calibrate()
+{
+    double latitude = LATITUDE;
+    double longitude = LONGITUDE;
+    int timezoneoffset = TIMEZONEOFFSET;
+    int sunrise_hour;
+    int sunrise_minute;
+    int sunset_hour;
+    int sunset_minute;
+
+    constexpr double ZENITH = 90.833;
+
+    auto now = std::chrono::system_clock::now();
+    std::time_t tt = std::chrono::system_clock::to_time_t(now);
+
+    std::tm local{};
+#ifdef _WIN32
+    localtime_s(&local, &tt);
+#else
+    local = *std::localtime(&tt);
+#endif
+
+    int N = local.tm_yday;
+
+    auto Calculate = [&](bool sunrise)
+    {
+        double lng_hour = longitude / 15.0;
+
+        double t = sunrise ?
+            N + ((6.0 -lng_hour) / 24.0):
+            N + ((18.0 - lng_hour) / 24.0);
+        // I'm so confused at these numbers they seem arbitary but all the math says yes so idfk
+        double M = (0.9856 * t ) - 3.289;
+
+        double L = M
+            + 1.916 * SDL_sinf(M * SDL_PI_F /180.0)
+            + 0.020 * SDL_sinf(2.0* M * SDL_PI_F /180.0)
+            + 282.634;
+
+        //normalise
+        while (L < 0) L += 360;
+        while (L >= 360) L -= 360;
+
+        double RA = atan(0.91764 * SDL_tanf(L * SDL_PI_F /180.0)) * 180.0 / SDL_PI_F;
+
+        //normalise? I'm still not sure what RA is
+        while (RA < 0) RA += 360;
+        while (RA >= 360) RA -= 360;
+
+        double Lquadrant = floor(L / 90.0)* 90.0;
+        double RAquadrant = floor(RA/90.0) * 90.0;
+
+        RA += (Lquadrant - RAquadrant);
+        RA /= 15.0;
+
+        double sinDec = 0.39782 * SDL_sinf(L * SDL_PI_F /180.0);
+        double cosDec = SDL_cosf(asin(sinDec));
+
+        double cosH =
+            (SDL_cosf(ZENITH * SDL_PI_F /180.0)
+            - sinDec * SDL_sinf(latitude * SDL_PI_F /180.0))
+            / (cosDec * SDL_cosf(latitude * SDL_PI_F /180.0));
+
+        // dreaded forever sun/night
+        if (cosH > 1.0 || cosH < -1.0)
+            return -1.0;
+
+        double H;
+
+        if (sunrise)
+            H = 360.0 - acos(cosH) * 180.0 / SDL_PI_F;
+        else
+            H = acos(cosH) * 180.0 / SDL_PI_F;
+
+        H /= 15.0;
+
+        double T = H + RA - (0.6571* t) - 6.622;
+
+        double UT = T - lng_hour;
+
+        while (UT < 0) UT += 24;
+        while (UT >= 24) UT -= 24;
+
+        double localTime = UT + timezoneoffset;
+
+        while (localTime < 0) localTime += 24;
+        while (localTime >= 24) localTime -=24;
+
+        return localTime;
+    };
+
+    double rise = Calculate(true);
+    double set = Calculate(false);
+
+    if (rise < 0 || set < 0)
+    {
+        sunrise_hour = sunrise_minute = -1;
+        sunset_hour = sunset_minute = -1;
+        return;
+    }
+
+    sunrise_hour = static_cast<int>(rise);
+    sunrise_minute = static_cast<int>((rise - sunrise_hour) * 60.0 + 0.5);
+
+    if (sunrise_minute == 60)
+    {
+        sunrise_minute = 0;
+        sunrise_hour = (sunrise_hour + 1) % 24;
+    }
+
+    sunset_hour = static_cast<int>(set);
+    sunset_minute = static_cast<int>((set - sunset_hour) * 60.0 + 0.5);
+
+    if (sunset_minute == 60)
+    {
+        sunset_minute = 0;
+        sunset_hour = (sunset_hour + 1) % 24;
+    }
+
+    night_chunk = ((24 - sunset_hour) + (sunrise_hour))*2;
+
+    if (sunrise_minute >= 30)
+    {
+        night_chunk += 1;
+    }
+
+    if (sunset_minute >= 30)
+    {
+        night_chunk -= 1;
+    }
 }
